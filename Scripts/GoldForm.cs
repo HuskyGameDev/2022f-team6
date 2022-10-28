abb993f2314df1876a674698e11383f40b2cf9ef
Thomas Wilkins <90795014+TW225@users.noreply.github.com>
2022-10-28 16:45:53 -0400

    More work on gold tweaking and movement (buggy)
    Movement is looking a lot better now, but I think something with changing hitboxes with gold form is messing with jumps

--- Scripts/GoldForm.cs
+++ Scripts/GoldForm.cs
@@ -4,18 +4,28 @@ using UnityEngine;
 
 public class GoldForm : MonoBehaviour
 {
-    public float minScale = 1f;
-    public float normScale = 2f;
+    [SerializeField] float minScale = 1f;
+    [SerializeField] float normScale = 2f;
+    [SerializeField] float changeTimer = 1f;
 
-    
+    private float currentTimer = 0f;
+
+    private void FixedUpdate()
+    {
+        currentTimer -= Time.fixedDeltaTime;
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (CompareTag("Gold"))
+        if (CompareTag("Gold") && currentTimer <= 0)
         {
             //shrink the player
             if (collision.gameObject.CompareTag("shrink"))
             {
                 gameObject.transform.localScale = new Vector3(minScale, minScale, minScale);
+
+                //sets a timer to prevent rapidly changing back and forth, this should help stop weird behavior
+                currentTimer = changeTimer;
             }
             else
 
@@ -24,6 +34,7 @@ public class GoldForm : MonoBehaviour
             {
                 gameObject.transform.localScale = new Vector3(normScale, normScale, normScale);
             }
+
         }
         else //allows player to grow outside of gold, this prevents them from being trapped small if they change forms
         {
--- Scripts/NewMove.cs
+++ Scripts/NewMove.cs
@@ -4,13 +4,17 @@ using UnityEngine;
 
 public class NewMove : MonoBehaviour
 {
+    //these are meant to be edited in the unity editor, tweak them as you please
     [SerializeField] float speed = 1;
     [SerializeField] float jumpStrength = 1;
     [SerializeField] float maxJumps = 1;
+    [SerializeField] float jumpTimer = 1f;
 
+    //should be safe to ignore these, used by functions below to keep track of various info
     private int currentJumps = 0;
     private Rigidbody2D rb;
-    public bool isGrounded = false;
+    private bool isGrounded = false;
+    private float currentJumpTimer = 0f;
 
     private void Start()
     {
@@ -19,16 +23,24 @@ public class NewMove : MonoBehaviour
 
     private void FixedUpdate()
     {
+        //this timer is to prevent "multi jumps" since this is a frame based movement without a timer you can get multiple
+        //jumps in at a time and it creates some weird/unpredictible behavior
+        currentJumpTimer -= Time.fixedDeltaTime;
+
         //handles horizontal movement
         rb.AddForce(new Vector2(Input.GetAxis("Horizontal") * speed, 0));
 
-        //if jumps are left and the player is on the ground jump
-        if (isGrounded && maxJumps > currentJumps)
+        //handles jumps by adding force when key is clicked and starting a timer (as well as keeping track of jumps)
+        if (Input.GetAxis("Jump") > 0 && isGrounded && maxJumps > currentJumps && currentJumpTimer <= 0)
         {
-            rb.AddForce(new Vector2(0, Input.GetAxis("Vertical") * jumpStrength), ForceMode2D.Impulse);
+            rb.AddForce(new Vector2(0, jumpStrength), ForceMode2D.Impulse);
+            currentJumps++;
+            currentJumpTimer = jumpTimer;
         }
     }
 
+    //checks if the player is on the ground
+    #region isGrounded
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("ground"))
@@ -42,7 +54,7 @@ public class NewMove : MonoBehaviour
         if (collision.gameObject.CompareTag("ground"))
         {
             isGrounded = false;
-            currentJumps--;
         }
     }
+    #endregion
 }

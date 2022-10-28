6140e0795939fe051ea82569284327b801033127
Thomas Wilkins <90795014+TW225@users.noreply.github.com>
2022-10-28 16:49:50 -0400

    Sprite changing
    I put a simple sprite change for gold form, mostly as a reminder (need to parse sprite)

--- Scripts/FormSwitching.cs
+++ Scripts/FormSwitching.cs
@@ -9,11 +9,17 @@ public class FormSwitching : MonoBehaviour
     [SerializeField] GameObject player;
     bool inGold = false;
     bool inPum = false;
-   /**KEY
-    1 = GoldForm
-   2 = Pummice form*/
+    public Sprite gold;
 
+    private SpriteRenderer sr;
+    /**KEY
+     1 = GoldForm
+    2 = Pummice form*/
 
+    private void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
 
     // Update is called once per frame
     void Update()
@@ -24,6 +30,9 @@ public class FormSwitching : MonoBehaviour
             player.tag = "Gold";
             inGold = true;
             Debug.Log("Gold = " + inGold);
+
+            //changes sprite
+            sr.sprite = gold;
         }
         //if the 1 key is pressed while the player is in gold form the tag becomes player
         else if (Input.GetKeyDown(KeyCode.Alpha1) && inGold)

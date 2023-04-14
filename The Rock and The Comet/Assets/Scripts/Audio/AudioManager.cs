a44f1d48e91f2a2f7d7daf030ee3416ece3df3f7
Thomas Wilkins <90795014+TW225@users.noreply.github.com>
2023-04-13 20:25:18 -0400

    Refactored Sound Manager
    Made sound manager contain 3 arrays for better organization of sounds

--- The Rock and The Comet/Assets/Scripts/Audio/AudioManager.cs
+++ The Rock and The Comet/Assets/Scripts/Audio/AudioManager.cs
@@ -6,13 +6,18 @@ using System;
 
 public class AudioManager : MonoBehaviour
 {
-    [SerializeField] Sound[] sounds;
+    private Sound[] sounds;
+    [SerializeField] Sound[] ambientSounds;
+    [SerializeField] Sound[] playerSounds;
+    [SerializeField] Sound[] music;
 
     AudioSource musicSource;
 
     private void Start()
     {
         musicSource = this.gameObject.AddComponent<AudioSource>();
+
+        combineArrays();
 
         //setup all the sounds
         foreach (Sound s in sounds)
@@ -37,6 +42,33 @@ public class AudioManager : MonoBehaviour
 
             s.source.volume = s.volume;
             s.source.loop = s.loop;
+        }
+    }
+
+    //this probably isn't effecient but its easy, might optimize it later
+    private void combineArrays()
+    {
+        sounds = new Sound[ambientSounds.Length + playerSounds.Length + music.Length];
+
+        int i = 0;
+
+        foreach(Sound s in ambientSounds)
+        {
+            sounds[i] = ambientSounds[i++];
+        }
+
+        int j = 0;
+
+        foreach (Sound s in playerSounds)
+        {
+            sounds[i+j] = playerSounds[j++];
+        }
+
+        int k = 0;
+
+        foreach (Sound s in music)
+        {
+            sounds[i+j+k] = music[k++];
         }
     }
 
